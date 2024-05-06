12398e13351ebf5814c307d25439e4750155e58d
Viswanathan P <drummerviswa@gmail.com>
2024-05-06 22:21:03 +0530

    first

---
+++ views.sql
@@ -0,0 +1,24 @@
+-- To view views
+-- select * from view_name;
+
+-- To create a view
+-- create view view1 as select Name,Regno from student;
+
+-- With options 
+-- create view view_name as (select condition);
+-- create view view2 as select Name,Regno from student where Regno=2002;
+
+-- Update in view
+-- update view1 set Regno=9006 where Name="Rio";
+
+-- Insert into views
+-- insert into view_name values ();
+-- insert into view2 values ("Viswa",9001);
+
+-- Delete a row
+-- delete from view_name where condition
+-- delete from view1 where Regno=2002;
+
+-- Drop views
+-- drop view view_name;
+-- drop view view2;
